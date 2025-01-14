23359dc316c41580a2a4a125a326de5557bdba2b
pallavirawat <me@rawatpallavi.com>
2025-01-14 09:52:04 +0530

    Add scripts for spun up and down

---
+++ scripts/compose-down.sh
@@ -0,0 +1,6 @@
+sudo docker compose -p servie down
+sudo docker compose -p excalidraw down
+sudo docker compose -p librechat down
+sudo docker compose -p perplexica down
+
+
---
+++ scripts/compose-up.sh
@@ -0,0 +1,5 @@
+sudo docker compose -f ~/oss/Perplexica/docker-compose.yaml up -d
+sudo docker compose -f ~/oss/LibreChat/docker-compose.yml up -d
+sudo docker compose -f ~/oss/excalidraw/docker-compose.yml up -d
+sudo docker compose -f ~/personal/servie/docker-compose.yml up -d
+
